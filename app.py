5c7b04ce5b0c391bd92a32f00a11b8f8ab451163
astromodder <22250203+astromodder@users.noreply.github.com>
2023-04-23 21:49:39 -0400

    hit limit and rm some duplicate files

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import openai
-from flask import Flask, request, jsonify, render_template
+import requests
+from flask import Flask, request, jsonify, render_template, Response, session
 from flask_restful import Resource, Api
+from flask_session import Session
 from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from io import BytesIO
-from textblob import TextBlob
 
 # Load environment variables
 load_dotenv()
@@ -19,6 +20,12 @@ openai_temperature = float(os.getenv("OPENAI_TEMPERATURE", 0.5))
 
 # Initialize Flask application
 app = Flask(__name__)
+
+# Configure session
+app.config['SECRET_KEY'] = os.getenv("SESSION_SECRET_KEY", "supersecretkey")
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
 api = Api(app)
 
 enable_debug = os.getenv("ENABLE_DEBUG", "false").lower() == "true"
@@ -40,18 +47,29 @@ def read_pdf(pdf_stream):
 
 
 def analyze_resume(resume_text):
-    # Analyze the resume using OpenAI GPT API
     prompt = f"Analyze the following resume: {resume_text}"
-    response = openai.ChatCompletion.create(
-        model=openai_engine,
-        messages=[{"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "user", "content": prompt}],
-        max_tokens=openai_max_tokens,
-        n=1,
-        stop=None,
-        temperature=openai_temperature,
+    response = requests.post(
+        f"https://api.openai.com/v1/engines/{openai_engine}/completions",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {openai_api_key}",
+        },
+        json={
+            "prompt": prompt,
+            "max_tokens": openai_max_tokens,
+            "n": 1,
+            "stop": None,
+            "temperature": openai_temperature,
+        },
+        stream=True,
     )
-    return response.choices[0].message['content'].strip()
+
+    return response
+
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'pdf'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 class ResumeApi(Resource):
@@ -82,6 +100,37 @@ def index():
     if request.method == "POST":
         analysis = ResumeApi().post()
     return render_template("index.html", analysis=analysis)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if 'resume' not in request.files:
+        return jsonify(error='No resume file provided'), 400
+
+    resume_file = request.files['resume']
+    if not allowed_file(resume_file.filename):
+        return jsonify(error='Invalid file type'), 400
+
+    resume_text = read_pdf(resume_file.stream)
+    session['resume_text'] = resume_text
+
+    return jsonify(status='ok')
+
+
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    resume_text = session.get('resume_text', None)
+    if not resume_text:
+        return jsonify(error='No resume text found'), 400
+
+    response = analyze_resume(resume_text)
+
+    def generate():
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                yield chunk
+
+    return Response(generate(), content_type="text/event-stream")
 
 
 if __name__ == "__main__":
